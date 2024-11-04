ccd57e70d5b7aeabadcaf4e2870c0cdc0e1982da
David Ku <dku7@proton.me>
2024-11-04 16:32:42 +0000

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
