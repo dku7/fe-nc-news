c11c8e638a9fd4af70179dfa77fa85abf18326ff
David Ku <dku7@proton.me>
2024-11-05 11:39:41 +0000

    add constants for posting comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,10 @@ export const VOTE_STATUS_VOTING_IN_PROGRESS = "Submitting your vote...";
 export const VOTE_STATUS_VOTE_SUCCESSFUL = "Thank you for voting.";
 export const VOTE_STATUS_VOTE_UNSUCCESSFUL =
   "Sorry, your vote could not submitted. Please try again.";
+
+// posting comments
+export const COMMENT_STATUS_POST_IN_PROGRESS = "Adding your comment...";
+export const COMMENT_STATUS_POST_SUCCESSFUL =
+  "Thank you for leaving your comment.";
+export const COMMENT_STATUS_POST_UNSUCCESSFUL =
+  "Sorry, your comment could not added. Please try again.";
