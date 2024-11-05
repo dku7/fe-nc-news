3ccdc8ed6f48eadecef0edc0f3ce782113cad109
David Ku <dku7@proton.me>
2024-11-05 13:56:45 +0000

    conditionally render delete button if the comment author is the logged in user

--- src/components/CommentFooter.jsx
+++ src/components/CommentFooter.jsx
@@ -1,12 +1,21 @@
 import { XMarkIcon } from "@heroicons/react/20/solid";
+import { useContext } from "react";
+import { LoggedInUserContext } from "../contexts/LoggedInUser";
 
-const CommentFooter = ({ comment }) => (
-  <div className="flex justify-between">
-    <span className="">votes: {comment.votes}</span>
+const CommentFooter = ({ comment }) => {
+  const { loggedInUser } = useContext(LoggedInUserContext);
+  const showDeleteButton = () => (
     <button title="Delete comment">
       <XMarkIcon className="size-6 text-black" />
     </button>
-  </div>
-);
+  );
+
+  return (
+    <div className="flex justify-between">
+      <span className="">votes: {comment.votes}</span>
+      {loggedInUser === comment.author ? showDeleteButton() : null}
+    </div>
+  );
+};
 
 export default CommentFooter;
