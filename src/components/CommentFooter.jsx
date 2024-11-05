03c1ec0c10d8d6aecee72a5945cb5bc28e0f976a
David Ku <dku7@proton.me>
2024-11-05 14:06:02 +0000

    add constants and delete state variables

--- src/components/CommentFooter.jsx
+++ src/components/CommentFooter.jsx
@@ -1,13 +1,31 @@
 import { XMarkIcon } from "@heroicons/react/20/solid";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { LoggedInUserContext } from "../contexts/LoggedInUser";
+import {
+  COMMENT_STATUS_DELETE_IN_PROGRESS,
+  COMMENT_STATUS_DELETE_SUCCESSFUL,
+  COMMENT_STATUS_DELETE_UNSUCCESSFUL,
+} from "../utils/constants";
 
 const CommentFooter = ({ comment }) => {
   const { loggedInUser } = useContext(LoggedInUserContext);
+  const [deleteStatus, setDeleteStatus] = useState("");
+  const [isDeleteEnabled, setIsDeleteEnabled] = useState(true);
+
+  const handleDeleteComment = () => {
+    setDeleteStatus(COMMENT_STATUS_DELETE_IN_PROGRESS);
+  };
+
   const showDeleteButton = () => (
-    <button title="Delete comment">
-      <XMarkIcon className="size-6 text-black" />
-    </button>
+    <div className="text-right">
+      <button
+        title="Delete comment"
+        disabled={!isDeleteEnabled}
+        onClick={handleDeleteComment}>
+        <XMarkIcon className="size-6 text-black" />
+      </button>
+      <p>{deleteStatus}</p>
+    </div>
   );
 
   return (
