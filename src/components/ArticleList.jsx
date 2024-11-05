af0067c491e1c72812956992ddfb31c990fe0858
David Ku <dku7@proton.me>
2024-11-05 16:45:58 +0000

    set sort_by query parameters via drop down box

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -5,7 +5,7 @@ import ErrorDisplay from "./ErrorDisplay";
 import ArticleCard from "./ArticleCard";
 import ArticleSorter from "./ArticleSorter";
 
-const ArticleList = ({ searchParams }) => {
+const ArticleList = ({ searchParams, setSearchParams }) => {
   const DEFAULT_LIMIT = 1000;
 
   const [articles, setArticles] = useState([]);
@@ -13,14 +13,27 @@ const ArticleList = ({ searchParams }) => {
   const [isError, setIsError] = useState(false);
   const [topic, setTopic] = useState("");
 
+  const handleSortByChange = (sortBy) => {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set("sort_by", sortBy);
+
+    setSearchParams(newParams);
+  };
+
   useEffect(() => {
-    const queryTopic = searchParams.get("topic");
-    const queryParams = { topic: queryTopic, limit: DEFAULT_LIMIT };
-
-    setTopic(queryTopic);
-
     setIsLoading(true);
     setIsError(false);
+
+    const queryTopic = searchParams.get("topic");
+    const querySortBy = searchParams.get("sort_by");
+
+    const queryParams = {
+      topic: queryTopic,
+      sort_by: querySortBy,
+      limit: DEFAULT_LIMIT,
+    };
+
+    setTopic(queryTopic);
 
     getArticles(queryParams)
       .then((articles) => setArticles(articles))
@@ -32,14 +45,16 @@ const ArticleList = ({ searchParams }) => {
   if (isError) return <ErrorDisplay />;
 
   return (
-    <div class="m-5">
+    <div className="m-5">
       <main>
         <header>
           <h2 className="text-3xl font-semibold capitalize">
             {topic ? topic : "All articles"}
           </h2>
         </header>
-        <ArticleSorter />
+        <div className="my-4">
+          <ArticleSorter handleSortByChange={handleSortByChange} />
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
           {articles.map((article) => (
             <ArticleCard key={article.article_id} article={article} />
--- src/components/ArticleSorter.jsx
+++ src/components/ArticleSorter.jsx
@@ -1,3 +1,39 @@
-const ArticleSorter = () => <p>Article Sorter</p>;
+import { useState } from "react";
+
+const ArticleSorter = ({ handleSortByChange }) => {
+  const sortOptions = [
+    { caption: "Date", value: "created_at" },
+    { caption: "Comments", value: "comment_count" },
+    { caption: "Votes", value: "votes" },
+  ];
+
+  const [sortBy, setSortBy] = useState("created_at");
+
+  const onSelectChange = (event) => {
+    const sortBy = event.target.value;
+
+    setSortBy(sortBy);
+    handleSortByChange(sortBy);
+  };
+
+  return (
+    <div>
+      <label htmlFor="sort-by" className="mr-4">
+        Sort by:
+      </label>
+      <select
+        className="border rounded px-2"
+        value={sortBy}
+        onChange={onSelectChange}
+        id="sort-by">
+        {sortOptions.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.caption}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default ArticleSorter;
