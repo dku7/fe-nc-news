1d9efc337ad4b1f355811cea67d028496dd3d0a5
David Ku <dku7@proton.me>
2024-11-05 11:40:06 +0000

    create basic form on CommentAdder

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -4,7 +4,24 @@ import { LoggedInUserContext } from "../contexts/LoggedInUser";
 const CommentAdder = () => {
   const { loggedInUser } = useContext(LoggedInUserContext);
 
-  return <p>Comment Adder</p>;
+  return (
+    <div>
+      <form>
+        <label htmlFor="comment-input">Add comment:</label>
+        <textarea
+          className="block border rounded w-full resize-none mt-2 mb-4"
+          name="comment-input"
+          id="comment-input"
+        />
+        <button
+          className="border rounded px-4 bg-gray-800 text-white mb-2"
+          type="submit">
+          Add
+        </button>
+      </form>
+      <p>feedback here</p>
+    </div>
+  );
 };
 
 export default CommentAdder;
