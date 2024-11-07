56880a8712e9273641fb5ef2e3f19cf1f48fab68
David Ku <dku7@proton.me>
2024-11-07 20:45:05 +0000

    add animated spinner

--- src/components/LoadingDisplay.jsx
+++ src/components/LoadingDisplay.jsx
@@ -1,6 +1,9 @@
 const LoadingDisplay = () => (
-  <div className="text-center">
-    <h2 className="text-lg font-semibold">Loading...</h2>
+  <div className="text-center pt-20">
+    <div
+      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status"></div>
+    <h2 className="text-lg">Loading, please wait...</h2>
   </div>
 );
 
