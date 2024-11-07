9d69141de3b0d443137494fe1f41ba9d1e6e932d
David Ku <dku7@proton.me>
2024-11-07 20:07:51 +0000

    remove setting username from App

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,16 @@ import { useContext, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Article from "./components/Article";
-import { LoggedInUserContext } from "./contexts/LoggedInUser";
 import NotFound from "./components/NotFound";
 import { SmallScreenContext } from "./contexts/SmallScreen";
 
 const App = () => {
   const { setIsSmallScreen } = useContext(SmallScreenContext);
-  const { setLoggedInUser } = useContext(LoggedInUserContext);
 
   useEffect(() => {
     const handleResize = () => setIsSmallScreen(window.innerWidth <= 768);
 
     window.addEventListener("resize", handleResize);
-
-    setLoggedInUser("cooljmessy"), [];
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
