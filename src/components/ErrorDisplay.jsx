cfa486c6a4a1792b118c753046bf08601f5da2fe
David Ku <dku7@proton.me>
2024-11-07 21:34:31 +0000

    refactor ErrorDisplay

--- src/components/ErrorDisplay.jsx
+++ src/components/ErrorDisplay.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import unamusedDog from "../assets/unamused-dog.jpg";
+
 const ErrorDisplay = () => (
-  <div className="text-center">
-    <h2 className="text-lg font-semibold">Something went wrong...</h2>
-  </div>
+  <>
+    <main className="text-center mt-20">
+      <header>
+        <h2 className="font-bold text-2xl">Oh no! Something went wrong</h2>
+      </header>
+      <img
+        className="h-2/3 lg:h-1/2 block mx-auto my-20"
+        src={unamusedDog}
+        alt="an unamused looking dog"
+      />
+    </main>
+  </>
 );
 
 export default ErrorDisplay;
