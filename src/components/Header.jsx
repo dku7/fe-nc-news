77cc641d5b08e4dcc06ac28faf470125aad35158
David Ku <dku7@proton.me>
2024-11-07 09:34:05 +0000

    restyle header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,20 @@
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
 const Header = () => (
-  <header className="text-center bg-gradient-to-b from-sky-700 to-sky-900 p-10 ">
-    <h1 className="text-4xl md:text-5xl font-bold mb-5 text-white">NC News</h1>
+  <header className="flex bg-gradient-to-b from-sky-700 to-sky-900 ">
+    <div className="p-4">
+      <button>
+        <Bars3Icon className="size-8 text-white md:hidden" />
+      </button>
+    </div>
+    <div className="w-full text-center py-4">
+      <h1 className="text-4xl md:text-5xl font-bold text-white p-4">
+        <span className="border bg-gray-50 rounded-l-lg rounded-r px-2 text-sky-700">
+          NC
+        </span>{" "}
+        News
+      </h1>
+    </div>
   </header>
 );
 
