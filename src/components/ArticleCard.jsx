64893424bb51a1a77645edfeaca6945969d181af
David Ku <dku7@proton.me>
2024-11-08 15:39:24 +0000

    adjust min size of card due to problems with pagination when there is a single item returned

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,7 +3,7 @@ import { formatDateTime } from "../utils/utils";
 import { Link } from "react-router-dom";
 
 const ArticleCard = ({ article }) => (
-  <div className="h-[424px] w-full md:w-1/2 lg:w-1/3">
+  <div className="h-[424px] w-full md:w-1/2 lg:w-1/3 lg:min-w-[340px]">
     <div className="m-4 h-fit overflow-hidden rounded-md bg-white shadow-md hover:shadow-2xl">
       <Link
         aria-label={`Read more about ${article.title}`}
@@ -17,17 +17,15 @@ const ArticleCard = ({ article }) => (
       </Link>
       <div className="px-4 py-2">
         <div className="mb-2 min-h-20">
-          <heading>
-            <h2>
-              <Link
-                className="hover:text-brand-secondary font-semibold hover:underline md:line-clamp-2"
-                aria-label={`Read more about ${article.title}`}
-                to={`/articles/${article.article_id}`}
-              >
-                {article.title}
-              </Link>
-            </h2>
-          </heading>
+          <h2>
+            <Link
+              className="hover:text-brand-secondary font-semibold hover:underline md:line-clamp-2"
+              aria-label={`Read more about ${article.title}`}
+              to={`/articles/${article.article_id}`}
+            >
+              {article.title}
+            </Link>
+          </h2>
         </div>
         <div>
           <Link
