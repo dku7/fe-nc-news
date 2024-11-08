89706be1aae6fca2b362825c9e9aebb96e694719
David Ku <dku7@proton.me>
2024-11-08 17:59:37 +0000

    make tooltip of order buttons dynamic depending on the currently selected sort by option

--- src/components/SortButton.jsx
+++ src/components/SortButton.jsx
@@ -4,28 +4,25 @@ import {
   QUERY_PARAM_ORDER_BY_DESC,
 } from "../utils/constants";
 
-const SortButton = ({ direction, currentValue, onOrderChange }) => {
+const SortButton = ({ direction, currentValue, tooltip, onOrderChange }) => {
   const cssClassName =
     direction === currentValue
       ? "sort-button__icon--selected"
       : "sort-button__icon--unselected";
 
   let svgComponent = <></>;
-  let buttonTitle = "";
 
   switch (direction) {
     case QUERY_PARAM_ORDER_BY_ASC:
       svgComponent = <ArrowUpIcon className={cssClassName} />;
-      buttonTitle = "Sort ascending";
       break;
     case QUERY_PARAM_ORDER_BY_DESC:
       svgComponent = <ArrowDownIcon className={cssClassName} />;
-      buttonTitle = "Sort descending";
       break;
   }
 
   return (
-    <button title={buttonTitle} onClick={() => onOrderChange(direction)}>
+    <button title={tooltip} onClick={() => onOrderChange(direction)}>
       {svgComponent}
     </button>
   );
