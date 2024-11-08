fa3009de71aac07015fa85f31fb4c32224f835d1
David Ku <dku7@proton.me>
2024-11-08 11:24:30 +0000

    additional tailwind configuration to build output styles.css file from input.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "./styles.css";
 import App from "./App.jsx";
 import { LoggedInUserProvider } from "./contexts/LoggedInUser.jsx";
 import { TopicsListProvider } from "./contexts/TopicsList.jsx";
@@ -18,5 +18,5 @@ createRoot(document.getElementById("root")).render(
         </SmallScreenProvider>
       </TopicsListProvider>
     </LoggedInUserProvider>
-  </StrictMode>
+  </StrictMode>,
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "brand-primary": "#002642",
+        "brand-secondary": "#840032",
+        "brand-tertiary": "#e59500",
+      },
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
