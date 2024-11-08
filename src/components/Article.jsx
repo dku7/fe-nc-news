9d577d1645c05f6a46b94af526c308e70d7e8b62
David Ku <dku7@proton.me>
2024-11-08 10:06:03 +0000

    adjust margins based on screen size

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -38,7 +38,7 @@ const Article = () => {
 
   return (
     <MainContainer article={article}>
-      <main className="m-10 w-full md:w-4/5 sm:px-8">
+      <main className="mx-2 mt-4 w-full sm:px-8 md:mx-28 md:w-4/5 lg:mx-60">
         <section>
           <ArticleHeader article={article} />
           <article className="py-4">
