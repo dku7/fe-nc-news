8a23fd32ffb827c16f7b3236e19b2810c227dabf
David Ku <dku7@proton.me>
2024-11-09 15:15:52 +0000

    create functions to get title and image depending on whether they should be clickable

--- src/components/ArticleHeader.jsx
+++ src/components/ArticleHeader.jsx
@@ -1,39 +1,60 @@
 import { formatDateTime } from "../utils/utils";
 import { Link } from "react-router-dom";
 
-const ArticleHeader = ({ article, canClickTitle }) => (
-  <div>
-    <img
-      className="w-full rounded-t-md object-cover"
-      alt={`User posted image about ${article.topic}`}
-      src={article.article_img_url}
-    />
+const ArticleHeader = ({ article, canClickTitle }) => {
+  const getImage = () => {
+    const articleImage = (
+      <img
+        className="w-full rounded-t-md object-cover"
+        alt={`User posted image about ${article.topic}`}
+        src={article.article_img_url}
+      />
+    );
 
-    <header>
-      <h2 className="my-4 text-2xl font-bold">
-        {canClickTitle ? (
-          <Link
-            className="hover:text-brand-secondary hover:underline"
-            to={`/articles/${article.article_id}`}
-          >
-            {article.title}
-          </Link>
-        ) : (
-          article.title
-        )}
-      </h2>
-    </header>
-    <div>
+    return canClickTitle ? (
       <Link
-        className="text-brand-secondary mb-2 hover:underline"
-        to={`/?topic=${article.topic}`}
+        aria-label={`Read more about ${article.title}`}
+        className="hover:text-brand-secondary hover:underline"
+        to={`/articles/${article.article_id}`}
       >
-        {article.topic}
+        {articleImage}
       </Link>
-      <p className="article__author">{article.author}</p>
-      <p>{formatDateTime(article.created_at)}</p>
+    ) : (
+      articleImage
+    );
+  };
+
+  const getTitle = () =>
+    canClickTitle ? (
+      <Link
+        aria-label={`Read more about ${article.title}`}
+        className="hover:text-brand-secondary hover:underline"
+        to={`/articles/${article.article_id}`}
+      >
+        {article.title}
+      </Link>
+    ) : (
+      article.title
+    );
+
+  return (
+    <div>
+      {getImage()}
+      <header>
+        <h2 className="my-4 text-2xl font-bold">{getTitle()}</h2>
+      </header>
+      <div>
+        <Link
+          className="text-brand-secondary mb-2 hover:underline"
+          to={`/?topic=${article.topic}`}
+        >
+          {article.topic}
+        </Link>
+        <p className="article__author">{article.author}</p>
+        <p>{formatDateTime(article.created_at)}</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ArticleHeader;
