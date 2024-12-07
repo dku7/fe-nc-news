fb176652b269e88af472b9458d2e66530bb03591
David Ku <dku7@proton.me>
2024-12-07 22:10:41 +0000

    add useCallback to Paginator

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,8 +1,15 @@
+import { useCallback } from "react";
 import PaginationButton from "./PaginationButton";
 
 const Paginator = ({ handlePageChange, currentPage, isLastPage }) => {
-  const handlePrevPage = () => handlePageChange(-1);
-  const handleNextPage = () => handlePageChange(1);
+  const handlePrevPage = useCallback(
+    () => handlePageChange(-1),
+    [handlePageChange],
+  );
+  const handleNextPage = useCallback(
+    () => handlePageChange(1),
+    [handlePageChange],
+  );
 
   return (
     <div className="mb-10 mt-10 flex">
