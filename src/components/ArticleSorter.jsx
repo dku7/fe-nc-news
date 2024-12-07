06c1e945f891f0cb4937a08313bef5924c21f488
David Ku <dku7@proton.me>
2024-12-07 21:55:55 +0000

    add useMemo and useCallback hooks

--- src/components/ArticleSorter.jsx
+++ src/components/ArticleSorter.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useMemo } from "react";
 import {
   QUERY_PARAM_SORT_BY,
   QUERY_PARAM_ORDER_BY,
@@ -8,36 +9,44 @@ import Selector from "./Selector";
 import SortButton from "./SortButton";
 
 const ArticleSorter = ({ sortBy, orderBy, handleSortChange }) => {
-  const sortOptions = [
-    {
-      caption: "Article date",
-      value: "created_at",
-      ascTooltip: "Oldest to newest",
-      descTooltip: "Newest to oldest",
-    },
-    {
-      caption: "Number of comments",
-      value: "comment_count",
-      ascTooltip: "Lowest to highest",
-      descTooltip: "Highest to lowest",
-    },
-    {
-      caption: "Number of votes",
-      value: "votes",
-      ascTooltip: "Lowest to highest",
-      descTooltip: "Highest to lowest",
-    },
-  ];
+  const sortOptions = useMemo(
+    () => [
+      {
+        caption: "Article date",
+        value: "created_at",
+        ascTooltip: "Oldest to newest",
+        descTooltip: "Newest to oldest",
+      },
+      {
+        caption: "Number of comments",
+        value: "comment_count",
+        ascTooltip: "Lowest to highest",
+        descTooltip: "Highest to lowest",
+      },
+      {
+        caption: "Number of votes",
+        value: "votes",
+        ascTooltip: "Lowest to highest",
+        descTooltip: "Highest to lowest",
+      },
+    ],
+    [],
+  );
 
-  const onSelectChange = (event) =>
-    handleSortChange(QUERY_PARAM_SORT_BY, event.target.value);
+  const onSelectChange = useCallback(
+    (event) => handleSortChange(QUERY_PARAM_SORT_BY, event.target.value),
+    [handleSortChange],
+  );
 
-  const onOrderChange = (direction) =>
-    handleSortChange(QUERY_PARAM_ORDER_BY, direction);
+  const onOrderChange = useCallback(
+    (direction) => handleSortChange(QUERY_PARAM_ORDER_BY, direction),
+    [handleSortChange],
+  );
 
-  const currentSortOption = sortOptions.filter(
-    (option) => option.value === sortBy,
-  )[0];
+  const currentSortOption = useMemo(
+    () => sortOptions.filter((option) => option.value === sortBy)[0],
+    [sortBy, sortOptions],
+  );
 
   return (
     <div className="flex">
